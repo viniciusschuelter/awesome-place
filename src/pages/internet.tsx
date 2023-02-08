fa017068f2007c0341601b68061a62b72e569b30
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-02-08 08:27:47 -0300

    feat(internet): feat new page internet

---
+++ src/pages/internet.tsx
@@ -0,0 +1,37 @@
+import data from '__mocks__/data/data.json';
+
+import CardCity from '@/components/CardCity';
+import { Meta } from '@/layouts/Meta';
+import type {
+  AwesomePlaceDataResponse,
+  CitiesInterface,
+} from '@/models/awesome-place.model';
+import { Main } from '@/templates/Main';
+
+const Internet = (props: AwesomePlaceDataResponse) => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Top cost places"
+          description="Top cost places choose by community."
+        />
+      }
+    >
+      <ul className="w-100 container mx-auto flex flex-wrap gap-3 lg:w-4/5">
+        {props?.cities
+          ?.sort((a, b) => b.internet_score - a.internet_score)
+          .slice(0, 10)
+          .map((city: CitiesInterface) => (
+            <CardCity key={city.slug} city={city}></CardCity>
+          ))}
+      </ul>
+    </Main>
+  );
+};
+
+export async function getServerSideProps() {
+  return { props: { ...data } };
+}
+
+export default Internet;
